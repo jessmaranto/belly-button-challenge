3ca89d87989e8357b2aa0f40e43d16a6dec7e043
jessmaranto <jessicamaranto@msn.com>
2024-10-02 14:02:45 -0600

    most current

--- app.js
+++ app.js
@@ -24,77 +24,68 @@ function buildMetadata(sample) {
 
 // function to build both charts
 function buildCharts(sample) {
-  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-
-    // Get the samples field
-    const samples = data.samples;
-
-    // Filter the samples for the object with the desired sample number
-    const result = samples.filter(sampleData => sampleData.id === sample)[0];
-
-    // Get the otu_ids, otu_labels, and sample_values
-    const otu_ids = result.otu_ids;
-    const otu_labels = result.otu_labels;
-    const sample_values = result.sample_values;
-
-    // Build a Bubble Chart
-    const bubbleData = [{
+    d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+      const samples = data.samples;
+      const result = samples.filter(sampleData => sampleData.id === parseInt(sample))[0];
+  
+      if (!result) {
+        console.error(`Sample ${sample} not found`);
+        return;
+      }
+  
+      const otu_ids = result.otu_ids;
+      const otu_labels = result.otu_labels;
+      const sample_values = result.sample_values;
+  
+      // Build a Bubble Chart
+      const bubbleData = [{
         x: otu_ids,
         y: sample_values,
         text: otu_labels,
         mode: 'markers',
         marker: {
-            size: sample_values,
-            color: otu_ids,
-            colorscale: 'Earth'
+          size: sample_values,
+          color: otu_ids,
+          colorscale: 'Earth'
         }
-    }];
-    const bubbleLayout = {
+      }];
+      const bubbleLayout = {
         title: { 
           text: "Bacteria Cultures Per Sample",
-        x: 0.05 // Set the title position to the left
-      },
+          x: 0.05
+        },
         margin: { t: 30, l: 60 },
         hovermode: "closest",
         xaxis: { title: "OTU ID" },
         yaxis: { title: "Number Of Bacteria" },
         autosize: true, 
-      font: { family: "Times" } 
-    };
-    // Render the Bubble Chart
-    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
-
-    // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-// Map otu_ids to a list of strings for y-ticks
-let yTicks = otu_ids.map(id => `OTU ${id}`);
-
-// Sort and slice the data to get the top 10 values
-let sortedData = sample_values.map((value, index) => {
-    return { value: value, id: otu_ids[index], label: otu_labels[index] };
-}).sort((a, b) => b.value - a.value).slice(0, 10);
-
-// Reverse the sorted data for Plotly
-sortedData.reverse();
-
-// Prepare data for Plotly
-let barDataArray = [{
-    x: sortedData.map(data => data.value),
-    y: sortedData.map(data => `OTU ${data.id}`), // Use the mapped y-ticks
-    type: 'bar',
-    text: sortedData.map(data => data.label), // Tooltips
-    orientation: 'h' // Horizontal bar chart
-}];
-
-// Layout for the bar chart
-let barLayout = {
-    title: 'Top 10 Bateria Cultures Found',
-    xaxis: { title: 'Number of Bacteria' },
-    yaxis: { title: 'OTU IDs' }
-};
-
-    // Render the Bar Chart
-    Plotly.newPlot('bar', barDataArray, barLayout);
-
+        font: { family: "Times" } 
+      };
+      Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+  
+      // Prepare data for the Bar Chart
+      let sortedData = sample_values.map((value, index) => {
+        return { value: value, id: otu_ids[index], label: otu_labels[index] };
+      }).sort((a, b) => b.value - a.value).slice(0, 10).reverse();
+  
+      let barDataArray = [{
+        x: sortedData.map(data => data.value),
+        y: sortedData.map(data => `OTU ${data.id}`),
+        type: 'bar',
+        text: sortedData.map(data => data.label),
+        orientation: 'h'
+      }];
+  
+      let barLayout = {
+        title: 'Top 10 Bacteria Cultures Found',
+        xaxis: { title: 'Number of Bacteria' },
+        yaxis: { title: 'OTU IDs' }
+      };
+    
+      Plotly.newPlot('bar', barDataArray, barLayout);
+    
+    }); // Closing brace for d3.json
+  }
 // Function to run on page load
 function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
@@ -110,7 +101,7 @@ function init() {
     // option for each sample name.
     sampleNames.forEach((sample) => {
         dropdownMenu.append("option")
-                    .text(sample) /
+                    .text(sample)
                     .property("value", sample); 
       });
 
@@ -131,4 +122,4 @@ function optionChanged(newSample) {
 }
 
 // Initialize the dashboard
-init();
+init(); 
